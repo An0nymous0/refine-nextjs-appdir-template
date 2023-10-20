f5e0ad8b2013b02f591415c583e6ef5bfe62914d
Xu Gao <398058068@qq.com>
2023-10-20 18:20:53 +0800

    add refine devtools

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,51 @@
 "use client";
+import { DevtoolsProvider, DevtoolsPanel } from "@refinedev/devtools";
 
-import {Inter} from 'next/font/google'
-import './globals.css'
-import {Refine} from "@refinedev/core";
-import {authProvider} from "@/lib/authProvider";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Refine } from "@refinedev/core";
+import { authProvider } from "@/lib/authProvider";
 import routerProvider from "@refinedev/nextjs-router/app";
 import dataProvider from "@refinedev/simple-rest";
-import {Toaster} from "@/components/ui/toaster"
-import {notificationProvider} from "@/lib/notificationProvider";
+import { Toaster } from "@/components/ui/toaster";
+import { notificationProvider } from "@/lib/notificationProvider";
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-        <body className={inter.className}>
-        <Refine
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <DevtoolsProvider>
+          <Refine
             authProvider={authProvider}
             routerProvider={routerProvider}
             dataProvider={dataProvider("API_URL")}
             resources={[
-                {
-                    name: "posts",
-                    list: "/posts",
-                },
-                {
-                    name: "welcome",
-                    list: "/welcome",
-                },
+              {
+                name: "posts",
+                list: "/posts",
+              },
+              {
+                name: "welcome",
+                list: "/welcome",
+              },
             ]}
             options={{
-                syncWithLocation: true,
+              syncWithLocation: true,
             }}
             notificationProvider={notificationProvider}
-        >
+          >
             {children}
-        </Refine>
-        <Toaster/>
-        </body>
-        </html>
-    )
+          </Refine>
+          <DevtoolsPanel />
+        </DevtoolsProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
 }
