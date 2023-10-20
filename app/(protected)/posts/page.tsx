f05a0781255e6678cd5fc1a2fbdd7133d96aad19
Xu Gao <398058068@qq.com>
2023-10-20 18:27:28 +0800

    Optimize page

--- app/(protected)/posts/page.tsx
+++ app/(protected)/posts/page.tsx
@@ -2,18 +2,33 @@
 
 import {useLogout} from "@refinedev/core";
 import {getSession} from "next-auth/react";
+import {Button} from "@/components/ui/button";
+import {useState} from "react";
 
 export default function Posts() {
+    const [session, setSession] = useState({})
     const {mutate: logout} = useLogout();
     const getSessions = () => {
         getSession().then(e =>
-            console.log("Posts-getSession()", e)
+            setSession({...e})
         );
     }
     return (
         <>
-            <button onClick={() => logout()}>退出</button>
-            <button onClick={() => getSessions()}>获取 session</button>
+            <nav className="py-4 px-4 sm:px-6 lg:px-4 xl:px-6 text-sm font-medium">
+                <ul className="flex space-x-3">
+                    <li>
+                        <Button onClick={() => getSessions()}>show session</Button>
+
+                    </li>
+                    <li>
+                        <Button onClick={() => logout()}>退出</Button>
+                    </li>
+                </ul>
+            </nav>
+            <p className="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">
+                <p>{JSON.stringify(session)}</p>
+            </p>
         </>
     );
 }
